8813e17b4c0d23bd5f284efdecb85aa292631e7c
SC-One <heydar.mahmoodi75@gmail.com>
2023-08-12 14:21:46 +0000

    added URI paths

--- QKrakenCpp.cpp
+++ QKrakenCpp.cpp
@@ -1,3 +1,31 @@
 #include "QKrakenCpp.h"
 
+#include <QString>
+#include <QUrl>
+
+static QThreadPool GLOBAL_THREAD_POOL = QThreadPool();
+struct ThreadPoolNumInitiator {
+  ThreadPoolNumInitiator(QThreadPool* pool, const int num = 10) {
+    Q_ASSERT(nullptr != pool);
+    pool->setMaxThreadCount(num);
+  }
+};
+
+static auto FAKE_USELESS_IN_CODE = ThreadPoolNumInitiator(&GLOBAL_THREAD_POOL);
+
+const QString QKrakenCpp::KRAKEN_REST_API_VERSION_DOC = "1.1.0";
+
+const QUrl QKrakenCpp::PUB_URL = QUrl("https://api.kraken.com/0/public/");
+const QUrl QKrakenCpp::URI_TIME = PUB_URL.resolved(QUrl("Time"));
+const QUrl QKrakenCpp::URI_SYS_STATUS = PUB_URL.resolved(QUrl("SystemStatus"));
+const QUrl QKrakenCpp::URI_ASSETS = PUB_URL.resolved(QUrl("Assets"));
+const QUrl QKrakenCpp::URI_ASSET_PAIRS = PUB_URL.resolved(QUrl("AssetPairs"));
+const QUrl QKrakenCpp::URI_TICKER = PUB_URL.resolved(QUrl("Ticker"));
+const QUrl QKrakenCpp::URI_OHLC = PUB_URL.resolved(QUrl("OHLC"));
+const QUrl QKrakenCpp::URI_DEPTH = PUB_URL.resolved(QUrl("Depth"));
+const QUrl QKrakenCpp::URI_TRADES = PUB_URL.resolved(QUrl("Trades"));
+const QUrl QKrakenCpp::URI_SPREAD = PUB_URL.resolved(QUrl("Spread"));
+
+const QUrl QKrakenCpp::PV_URL = QUrl("https://api.kraken.com/0/private/");
+
 QKrakenCpp::QKrakenCpp() {}
--- QKrakenCpp.h
+++ QKrakenCpp.h
@@ -1,9 +1,32 @@
 #ifndef QKRAKENCPP_H
 #define QKRAKENCPP_H
 
+#include <QThread>
+#include <QThreadPool>
+#include <QUrl>
+
 #include "QKrakenCpp_global.h"
 
 class QKRAKENCPP_EXPORT QKrakenCpp {
+ public:
+  // when I write this library it was correspond to Rest API 1.1.0 in kraken
+  // documentations:
+  // https://docs.kraken.com/rest/
+  static const QString KRAKEN_REST_API_VERSION_DOC;
+
+  static const QUrl PUB_URL;
+  static const QUrl URI_TIME;
+  static const QUrl URI_SYS_STATUS;
+  static const QUrl URI_ASSETS;
+  static const QUrl URI_ASSET_PAIRS;
+  static const QUrl URI_TICKER;
+  static const QUrl URI_OHLC;
+  static const QUrl URI_DEPTH;
+  static const QUrl URI_TRADES;
+  static const QUrl URI_SPREAD;
+
+  static const QUrl PV_URL;
+
  public:
   QKrakenCpp();
 };
